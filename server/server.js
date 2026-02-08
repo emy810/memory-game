9582f8e74f725a916e6ad659adce7395e7c3e8dc
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-02-08 15:35:57 +0100

    database connected

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
-const express = require("express");
+import knexLibrary from "knex";
+import express from "express";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!!!");
+const dbFile = "../db/game.sqlite";
+
+const knex = knexLibrary({
+  client: "sqlite3",
+  connection: {
+    filename: "../db/game.sqlite",
+  },
+});
+
+app.get("/", async (request, response) => {
+  const cards = await knex.raw(`SELECT * FROM cards`);
+  response.json(cards);
 });
 
 app.listen(port, () => {
