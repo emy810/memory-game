21905a086dfeab94995fd803e737cf955a29100a
Emebet <emymak93@yahoo.com>
2026-02-21 14:44:08 +0100

    Add ES module --dirname and --filename support in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ import knexLibrary from "knex";
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 const app = express();
 const port = 3000;
